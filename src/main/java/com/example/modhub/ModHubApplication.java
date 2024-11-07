6b16b58252916e290e958bb54797010e175aa4b2
KitsuneSiho <frosty-sky@naver.com>
2024-11-07 22:35:54 +0900

    Initial commit

---
+++ src/main/java/com/example/modhub/ModHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.modhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/modhub/ModHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
